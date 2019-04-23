e4ce9b7c379087ff8296a6d9f120c32d594aee17
Piotr Przygoda <przygoda@gmail.com>
2019-04-23 20:20:38 +0200

    Finished

--- components/Contact.js
+++ components/Contact.js
@@ -1,24 +1,31 @@
 const Contact = React.createClass({
-	propTypes: {
-		item: React.PropTypes.object.isRequired,
-	},
+  propTypes: {
+    item: React.PropTypes.object.isRequired,
+  },
 
-	render: function () {
-		return (
-			<div className= {'contactItemSpace'}>
-				<div className= {'contactItem'}>
-					<img className={'contactImage'} src={'https://cdn4.iconfinder.com/data/icons/ionicons/512/icon-ios7-contact-outline-512.png'} />
-					<p className={'contactLabel'}>
-						Imię: {this.props.item.firstName}
-					</p>
-					<p className={'contactLabel'}>
-						Nazwisko: {this.props.item.lastName}
-					</p>
-					<a className= {'contactEmail'} href= {'mailto:' + this.props.item.email }>
-						{this.props.item.email}
-					</a>
-				</div>
-			</div>
-		)
-	},
+  render: function() {
+      const {
+          firstName,
+          lastName,
+          email,
+      } = this.props.item;
+
+      const imageURL = 'https://cdn4.iconfinder.com/data/icons/ionicons/512/icon-ios7-contact-outline-512.png';
+      
+    return (
+      <div className="contactItem">
+        <img className="contactImage" src={imageURL} />
+        <p className="contactLabel">
+          Imię: {firstName}
+        </p>
+        <p className="contactLabel">
+          Nazwisko: {lastName}
+        </p>
+        <a href={`mailto: ${email}`}>
+          {email}
+        </a>
+      
+      </div>
+    )
+  }
 });
